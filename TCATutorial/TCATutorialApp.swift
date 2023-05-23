3556d7d989fc58a54c3d6db0e1e6b289c3433909
masa950724mi <s100.iizuka.masami@gmail.com>
2023-05-23 13:37:10 +0900

    Initial Commit

---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by 飯塚政美 on 2023/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
